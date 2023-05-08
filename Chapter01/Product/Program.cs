fd2c97329107eca57dec33f16bbd6c4737505202
中澤 裕斗 <OIBC34255@ojs.ac.jp>
2023-05-08 12:06:24 +0900

    0508演習1

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -8,10 +8,28 @@ namespace ProductSample {
 
     class Program {
         static void Main(string[] args) {
-            Product karinto = new Product(123, "かりんとう", 180);
-            Product daihukumoti = new Product(123, "大福もち", 180);
-            Console.WriteLine("かりんとうの税込み価格："karinto.GetPriceIncludingTax());
-            Console.WriteLine("大福もちの税込み価格："daihukumoti.GetPriceIncludingTax());
+
+
+            #region P26のサンプルプログラム
+            //Product karinto = new Product(123, "かりんとう", 180);
+            //Product daihukumoti = new Product(123, "大福もち", 180);
+            //Console.WriteLine("かりんとうの税込み価格："+karinto.GetPriceIncludingTax());
+            //Console.WriteLine("大福もちの税込み価格："+daihukumoti.GetPriceIncludingTax());
+            #endregion
+
+
+            //DateTime date =new DateTime(2023,5,8);
+            DateTime date = DateTime.Today;
+
+            //10日後を決める
+            DateTime daysAfter10 = date.AddDays(10);
+            Console.WriteLine("10日後は" + daysAfter10.Day );
+
+            //10日前を決める
+            DateTime daysAgo10 = date.AddDays(-10);
+            Console.WriteLine("10日前は" + daysAgo10.Day );
+
+
         }
     }
 }
