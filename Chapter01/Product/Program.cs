4f5c89ec6d90db249d69129dc0740aa32036a55c
中澤 裕斗 <OIBC34255@ojs.ac.jp>
2023-05-09 10:00:20 +0900

    05009演習２

--- Chapter01/Product/Program.cs
+++ Chapter01/Product/Program.cs
@@ -17,7 +17,7 @@ namespace ProductSample {
             //Console.WriteLine("大福もちの税込み価格："+daihukumoti.GetPriceIncludingTax());
             #endregion
 
-
+            #region 演習１
             //DateTime date =new DateTime(2023,5,8);
             DateTime date = DateTime.Today;
 
@@ -31,7 +31,20 @@ namespace ProductSample {
             //10日前を決める
             DateTime daysAgo10 = date - ts1;
             Console.WriteLine("10日前は" + daysAgo10.Day );
+            #endregion
 
+            Console.WriteLine("西暦：");
+            int birthYear = int.Parse(Console.ReadLine());
+            Console.WriteLine("月：");
+            int birthMonth = int.Parse(Console.ReadLine());
+            Console.WriteLine("日：");
+            int birthDay = int.Parse(Console.ReadLine());
+
+
+            DateTime birth = new DateTime(birthYear, birthMonth, birthDay,0,0,0);
+            DateTime today = DateTime.Today;
+            TimeSpan timeSpan = today - birth;
+            Console.WriteLine("あなたは生まれてから今日まで{0}日です。", timeSpan.Days);
 
         }
     }
