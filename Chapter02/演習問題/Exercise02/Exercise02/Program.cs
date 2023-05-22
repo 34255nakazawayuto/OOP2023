2b554c4b874519dfad2e8ad44ca5278067eb5b37
中澤 裕斗 <OIBC34255@ojs.ac.jp>
2023-05-22 09:31:10 +0900

    2章

---
+++ Chapter02/演習問題/Exercise02/Exercise02/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length < 3){
+                return;
+            }
+            if(args.Length >= 1 && args[0] == "-toi"){
+                PrintMeterToInchList(int.Parse(args[1]),int.Parse(args[10]));
+            }
+        }
+        //フィートからメートルへの対応表を出力
+        private static void PrintInchToMeterList(int start, int stop) {
+            for (int inch = start; inch <= stop; inch++)
+            {
+                double meter = InchConverter.ToMeter(inch);
+                Console.WriteLine("{0} inch = {1:0.0000}m", inch, meter);
+            }
+        }
+
+        //メートルからフィートへの対応表を出力
+        private static void PrintMeterToFeetList(int start, int stop) {
+            for (int meter = 1; meter <= 10; meter++)
+            {
+                double feet = InchConverter.FromMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000}ft", meter, feet);
+            }
+        }
+    }
+}
+    }
+    }
+}
