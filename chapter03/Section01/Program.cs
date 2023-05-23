a30e71d8a8482f9186c35d41c8665f04ebfd4552
中澤 裕斗 <OIBC34255@ojs.ac.jp>
2023-05-23 09:23:57 +0900

    P87step5まで

--- chapter03/Section01/Program.cs
+++ chapter03/Section01/Program.cs
@@ -7,27 +7,15 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
 
-        public delegate bool Judgement(int value);//デリゲートの宣言
-
         static void Main(string[] args) {
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-           // Judgement judge = IsEven;
+            // Judgement judge = IsEven;
 
-            int count = Count(numbers, delegate(int n){ return n % 2 == 0; });
-           Console.WriteLine (count);
-        }
+            int count = numbers.Count(n => n % 2 == 0);//ラムダ式
 
-        static public int Count (int[] numbers, Predicate<int> judge) { 
 
-            int count = 0;
-
-            foreach (var n in numbers) {
-                if ( judge (n) == true )  {
-                    count++;
-                }
-            }    
-                return count;
-            }
+            Console.WriteLine(count);
         }
     }
+}
 
