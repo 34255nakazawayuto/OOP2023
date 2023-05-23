d5b0b934d291e865a7bb9e07cb277fe107a336fb
中澤 裕斗 <OIBC34255@ojs.ac.jp>
2023-05-23 11:54:12 +0900

    第３章演習問題１

---
+++ chapter03/Section04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section04 {
+    class Program {
+        static void Main(string[] args) {
+            var names = new List<string> {
+               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            //var quary = names.FindAll(s => s.Length <= 5).Select(s => s.ToLower());
+           // IEnumerable<string> quary = names.Where(s => s.Length <= 5);
+           var quary = names.Select(s => s.Length);
+
+            foreach (var s in quary){
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
