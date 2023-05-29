94c5b50798c736de9e2feb1c0def88275d398c4f
中澤 裕斗 <OIBC34255@ojs.ac.jp>
2023-05-29 12:09:03 +0900

    Section07

---
+++ chapter04/条件演算子'合体演算子/Section04/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section04 {
+    class Program {
+        static void Main(string[] args) {
+            #region 条件演算子
+            //var list = new List<int> { 10, 20, 30, 40 ,};
+            //var key = 40;
+            //var num = list.Contains(key) ? 1 : 0;//条件演算子・三項演算子
+            //Console.WriteLine(num);
+            #endregion
+            #region 合体演算子
+            //string code = "12345";
+            //var message = GetMessage(code) ?? DefaultMessage();
+            #endregion
+
+            #region null条件演算子
+            // Sale sale = new Sale{
+            //   Amount = 100,
+            //};
+            Sale sale = null;  
+
+            //int?　はnull許容型、「?.」はnull条件演算子
+            int? ret = sale?.Amount;
+            Console.WriteLine(ret);
+            #endregion
+        }
+        private static object GetMessage(object code) {
+            return null;
+        }
+        private static object DefaultMessage() {
+            return "DefaultMessage";
+        }
+    }
+    //売り上げクラス
+    public class Sale {
+        //店舗名
+        public string ShopName { get; set; }
+        //商品カテゴリー
+        public string ProductCategory { get; set; }
+        //売上高
+        public int Amount { get; set; }
+
+    }
+}
