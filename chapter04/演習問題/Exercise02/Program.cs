81ab3e9b620dec54ea8b164c9a9490d942f9089d
中澤 裕斗 <OIBC34255@ojs.ac.jp>
2023-05-31 11:03:45 +0900

    4,2,3

--- chapter04/演習問題/Exercise02/Program.cs
+++ chapter04/演習問題/Exercise02/Program.cs
@@ -23,12 +23,12 @@ namespace Exercise02 {
             Exercise2_2(ymCollection);
             Console.WriteLine("\n- 4.2.4 ---");
 
-           //4.2.4
+            //4.2.4
             Exercise2_4(ymCollection);
             Console.WriteLine("\n- 4.2.5 ---");
 
 
-           //4.2.5
+            //4.2.5
             Exercise2_5(ymCollection);
 
 
@@ -40,18 +40,29 @@ namespace Exercise02 {
                 Console.WriteLine(ym);
             }
         }
-
+        //4.2.3
         static YearMonth FindFirst21C(YearMonth[] yms) {
-            return 
+            foreach (var ym in yms) {
+                if (ym.Is21Century  ) {
+                    return ym;
+                }
+            }
+            return null;
         }
 
+            private static void Exercise2_4(YearMonth[] ymCollection) {
+                var yearmonth = FindFirst21C(ymCollection);
+                 if(yearmonth == null){
+                    Console.WriteLine("21正規のデータはありません");
+                 }
+                 else{
+                     Console.WriteLine(yearmonth);
+                 }
+            }
 
-        private static void Exercise2_4(YearMonth[] ymCollection) {
-            throw new NotImplementedException();
-        }
-
-        private static void Exercise2_5(YearMonth[] ymCollection) {
-            throw new NotImplementedException();
-        }
+            private static void Exercise2_5(YearMonth[] ymCollection) {
+                
+            }
     }
 }
+
