3ce98f3857644123cd3402e59f02f806a24c0ec1
中澤 裕斗 <OIBC34255@ojs.ac.jp>
2023-06-19 15:01:07 +0900

    設問１_Student クラス完成

--- TestProject/Test01/Student.cs
+++ TestProject/Test01/Student.cs
@@ -1,7 +1,7 @@
 ﻿namespace Test01 {
     class Student {
-        string Name;
-        string Subject;
-        int Score;
+        public string Name { get; set; }
+        public string Subject { get; set; }
+        public int Score { get; set; }
     }
 }
