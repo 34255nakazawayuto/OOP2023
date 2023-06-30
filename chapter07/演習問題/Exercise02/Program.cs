ab7bffbc69d1b8bdfde64b92e7fcc77579ddd3ba
中澤 裕斗 <OIBC34255@ojs.ac.jp>
2023-06-30 10:54:54 +0900

    Exercise02

---
+++ chapter07/演習問題/Exercise02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            var abbrs = new Abbreviations();
+
+            abbrs.Add("IOC", "国際オリンピック委員会");
+            abbrs.Add("NPT", "核拡散防止委員会");
+
+            //7.2.3
+            //上のAddメソッドで2つのオブジェクトを追加しているので
+            //読み込んだ単語数が+2が、Countの値になる
+            Console.WriteLine(abbrs.Count);
+            Console.WriteLine();
+
+            //7.2.3(Removeの呼び出し)
+            if (abbrs.Remove("NPT"))
+                Console.WriteLine(abbrs.Count);
+            if (!abbrs.Remove("NPT"))
+                Console.WriteLine("削除できません");
+            Console.WriteLine();
+            //7.2.4
+            //IEnumerable<>を実装したので,LINQが使える
+            foreach (var item in abbrs.Where(x=>x.Key.Length == 3)){
+                Console.WriteLine("{0}={1}",item.Key,item.Value);
+            }
+        }
+    }
+}
+
