d73311c250a9a88c1db2aa6da5f87442924b9a5a
中澤 裕斗 <OIBC34255@ojs.ac.jp>
2023-07-11 12:48:42 +0900

    a

--- TestProject/Test01/ScoreCounter.cs
+++ TestProject/Test01/ScoreCounter.cs
@@ -1,56 +1,48 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 
+
+
 namespace Test01 {
     class ScoreCounter {
         private IEnumerable<Student> _score;
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
-            public ScoreCounter(string filePath) {
-                _score = Readscore(filePath);
-            }
-            private static IEnumerable<Student> ReadSales(string filePath) {
-                var sales = new List<Student>();
-                var lines = File.ReadAllLines(filePath);
-                foreach (var line in lines)
-                {
-                    var items = line.Split(',');
-                    var score = new Student
-                    {
-                        Name = items[0],
-                        Subject = items[1],
-                        Score = int.Parse(items[2])
-                    };
-                    score.Add(score);
-                }
-                return score;
-            }
-
-
+            _score = ReadScore(filePath);
         }
 
-
-        //メソッドの概要： 
+        //メソッドの概要：点数データを読み込み、Scoreオブジェクトのリストを返す 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            List<Student> score = new List<Student>();
 
-
-
-
-
-
-
+            var students = new List<Student>();
+            var lines = File.ReadAllLines(filePath);
+            foreach (string line in lines){
+                var items = line.Split(',');
+                var student = new Student
+                {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2]),
+                };
+                students.Add(student);
+            }
+            return students;
         }
 
-        //メソッドの概要： 
+
+
+        //メソッドの概要：科目別合計点数を求める
         public IDictionary<string, int> GetPerStudentScore() {
 
-
-
-
-
-            
+            var dict = new SortedDictionary<string, int>();
+            foreach (Student student in _score){
+                if (dict.ContainsKey(student.Subject))
+                    dict[student.Subject] += student.Score;
+                else
+                    dict[student.Subject] = student.Score;
+            }
+            return dict;
         }
     }
 }
