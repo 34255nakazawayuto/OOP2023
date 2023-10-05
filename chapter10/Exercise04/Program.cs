428f8214012ac74f9d0d42bb19d9b5d9b8847cfb
中澤 裕斗 <OIBC34255@ojs.ac.jp>
2023-10-05 12:08:58 +0900

    chapter10 Exercise04  未完成

---
+++ chapter10/Exercise04/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise04 {
+    class Program {
+        static void Main(string[] args) {
+            var lines = File.ReadAllLines("sample.txt");
+
+            var m = lines.Replace("v4.0", "v5.0");
+
+
+            //書き込み
+            // File.WriteAllLines("sample.txt",);
+
+            // これ以降は確認用
+            var text = File.ReadAllText("sample.txt");
+            Console.WriteLine(text);
+        }
+    }
+}
