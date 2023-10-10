dccd729c02f6342efe2d49b27fabb1b0f9af1a99
中澤 裕斗 <OIBC34255@ojs.ac.jp>
2023-10-10 09:56:21 +0900

    Exercise04

--- chapter10/Exercise04/Program.cs
+++ chapter10/Exercise04/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Exercise04 {
@@ -10,11 +11,10 @@ namespace Exercise04 {
         static void Main(string[] args) {
             var lines = File.ReadAllLines("sample.txt");
 
-            var m = lines.Replace("v4.0", "v5.0");
-
+            var newline = lines.Select(s => Regex.Replace(s, @"\bversion\s*=\s*""v4.0""", @"version=""v5.0"""));
 
             //書き込み
-            // File.WriteAllLines("sample.txt",);
+             File.WriteAllLines("sample.txt",newline);
 
             // これ以降は確認用
             var text = File.ReadAllText("sample.txt");
