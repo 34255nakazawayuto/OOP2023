72da78a97699240aafde8a0c478435e30968e277
中澤 裕斗 <OIBC34255@ojs.ac.jp>
2023-10-18 12:02:02 +0900

    chapter17 距離

--- chapter17/Strategy/DistanceConverter/Properties/AssemblyInfo.cs
+++ chapter17/Strategy/DistanceConverter/Properties/AssemblyInfo.cs
@@ -4,25 +4,25 @@ using System.Runtime.InteropServices;
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
-// これらの属性値を変更してください。
-[assembly: AssemblyTitle("TextFileProcessor")]
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("DistanceConverter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TextFileProcessor")]
+[assembly: AssemblyProduct("DistanceConverter")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
 // 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
-// その型の ComVisible 属性を true に設定してください。
+// その型の ComVisible 属性を true に設定します。
 [assembly: ComVisible(false)]
 
 // このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
-[assembly: Guid("9a3549af-b560-4cca-b078-9bb679fc14b6")]
+[assembly: Guid("4e050608-e2f9-4cf1-a057-b472462532e1")]
 
-// アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
 //      メジャー バージョン
 //      マイナー バージョン
