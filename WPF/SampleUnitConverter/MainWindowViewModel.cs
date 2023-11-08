844ce9d750996c91f75e82399207d442294051b3
中澤 裕斗 <OIBC34255@ojs.ac.jp>
2023-11-08 11:13:59 +0900

    SampleUnitConverter  11/8

---
+++ WPF/SampleUnitConverter/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SampleUnitConverter {
+    public class MainWindowViewModel : ViewModel{
+        private double metricValue, imperialValue;
+
+        public ICommand ImperialUnitToMetric { get; private set; }
+        public ICommand MetricToImperialUnit { get; private set; }
+        public MetricUnit CurrentMetricUnit{ get;  set; }
+        public ImperialUnit CurrentImperialUnit { get;  set; }
+
+        public double MetricValue{
+            get{ return this.metricValue; }
+            set{
+                this.metricValue = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public double ImperialValue{
+            get{ return this.imperialValue; }
+            set{
+                this.imperialValue = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public MainWindowViewModel() {
+            this.CurrentMetricUnit = MetricUnit.Units.First();
+            this.CurrentImperialUnit = ImperialUnit.Units.First();
+            this.MetricToImperialUnit = new DelegateCommand(
+                () => this.ImperialValue = this.CurrentImperialUnit.FromMetricUnit(
+                   this.CurrentMetricUnit, this.MetricValue));
+
+            this.ImperialUnitToMetric = new DelegateCommand(
+               () => this.MetricValue = this.CurrentMetricUnit.FromImperialUnit(
+                   this.CurrentImperialUnit, this.ImperialValue));
+        }
+
+    }
+}
